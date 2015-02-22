81e0c4c6fc3283c60ec2778bcb778ce23218ea8f
kyle-v <kvaidyan@usc.edu>
2015-02-22 03:04:08 -0800

    Added autofocus
    yaaaasss

--- Assets/Resources/Scripts/PlayerControllerScript.cs
+++ Assets/Resources/Scripts/PlayerControllerScript.cs
@@ -8,6 +8,13 @@ public class PlayerControllerScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         camera = Camera.main;
+		bool focusModeSet;
+		focusModeSet = CameraDevice.Instance.SetFocusMode(  
+		                                                  CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		
+		if (!focusModeSet) {
+			Debug.Log("Failed to set focus mode (unsupported mode).");
+		}
 	}
 	
 	// Update is called once per frame
