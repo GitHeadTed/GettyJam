db42f7eda94051b2c2e43a63533433348fd27fcf
Steven Li <listeven@usc.edu>
2015-02-22 12:42:40 -0800

    activate objects through dialogue

--- Assets/Resources/Scripts/TextBubble.cs
+++ Assets/Resources/Scripts/TextBubble.cs
@@ -5,9 +5,12 @@ public class TextBubble : Clickable {
 
 	public string [] dialogue = { "First dialogue.", "Second dialogue" };
 	public bool [] textUnlocked = {true, true};
+    public GameObject[] objectEnable;
 	public int advanceToOnSolve = 0;
 	private TextMesh tm;
 	private int current;
+    private GameObject childTooltip;
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +18,7 @@ public class TextBubble : Clickable {
 		tm = GetComponent<TextMesh>();
 		current = 0;
 		tm.text = dialogue[current];
-
+        childTooltip = transform.FindChild("Tooltip").gameObject;
 	}
 	
 	// Update is called once per frame
@@ -29,7 +32,8 @@ public class TextBubble : Clickable {
 		if(current+1 < dialogue.Length && textUnlocked[current + 1]){
 			current++;
 			tm.text = dialogue[current];
-
+            if(objectEnable[current])
+                objectEnable[current].SetActive(true);
 			
 		}
 	}
